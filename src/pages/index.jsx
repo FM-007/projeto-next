aaebdf918e81917feea1c33aa10c01931b527d28
Felipe Moreira <felipe.g.silva@capgemini.com>
2021-11-26 16:59:52 -0300

    Configurado busca de dados no momento da construção .

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,28 @@
+import P from 'prop-types';
 import Head from 'next/head';
 import styled from 'styled-components';
+import config from '../config/index';
+import { mapData } from '../api/map-data';
 
 const Heading = styled.h1`
   background: ${({ theme }) => theme.colors.secondaryColor};
 `;
 
-export default function Home() {
-  return <Heading>Hello World</Heading>;
-}
+export default function Index({ data = null }) {
+  return <Heading>Felipe</Heading>;
+};
+
+export const getStaticProps = async () =>{
+  const raw = await fetch(config.url + config.defaultSlug);
+  const json = await raw.json();
+  const data = mapData(json)[0];
+  return {
+    props: {
+      data,
+    }
+  };
+};
+
+Index.propTypes = {
+  data: P.object,
+};
