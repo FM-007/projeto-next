9b572b066c3d6c6d6029ef40a3dd307f4511ab37
Felipe Moreira <felipe.g.silva@capgemini.com>
2021-11-26 15:15:37 -0300

    Configurado Jest, styled-components, storybook, eslint, prettier, editorconfig

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,10 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  background: ${({ theme }) => theme.colors.secondaryColor};
+`;
 
 export default function Home() {
-  return (
-    <h1>Hello World</h1>
-  )
+  return <Heading>Hello World</Heading>;
 }
