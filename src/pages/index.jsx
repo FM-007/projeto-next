9c7881c31e9261ec183c8acf49c789113a060a71
Felipe Moreira <felipe.g.silva@capgemini.com>
2021-11-29 12:09:25 -0300

    Introduzindo SSG com getStaticProps

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,25 @@
 import P from 'prop-types';
-import Head from 'next/head';
-import styled from 'styled-components';
-import config from '../config/index';
-import { mapData } from '../api/map-data';
 
-const Heading = styled.h1`
-  background: ${({ theme }) => theme.colors.secondaryColor};
-`;
+import config from '../config';
+import { mapData } from '../api/map-data';
+import Home from '../templates/Home';
 
 export default function Index({ data = null }) {
-  return <Heading>Felipe</Heading>;
+  console.log(data);
+  return <Home data={data} />;
 };
 
-export const getStaticProps = async () =>{
+export const getStaticProps = async () => {
   const raw = await fetch(config.url + config.defaultSlug);
   const json = await raw.json();
-  const data = mapData(json)[0];
+  const data = mapData(json);
   return {
     props: {
       data,
-    }
+    },
   };
 };
 
 Index.propTypes = {
-  data: P.object,
+  data: P.array
 };
